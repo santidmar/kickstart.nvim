e19ddc4089edd8018bf83c79982f8ea8daffef28
Santiago Martin <santi@lightspark.com>
2025-05-16 14:22:15 -0700

    helm + ts

--- init.lua
+++ init.lua
@@ -622,6 +622,9 @@ require('lazy').setup({
         bashls = {},
         graphql = {},
         kotlin_language_server = {},
+        ts_ls = {},
+        helm_ls = {},
+
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
