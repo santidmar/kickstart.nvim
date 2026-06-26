d6a4a615e5be03f7676c5e618905d65a471e664d
Santiago Martin <santi@grain.inc>
2026-06-26 12:08:04 -0700

    leader explore

--- init.lua
+++ init.lua
@@ -176,6 +176,10 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Open the netrw file explorer
+vim.keymap.set('n', '<leader>e', '<cmd>Explore<CR>', { desc = 'Open file [E]xplorer (netrw)' })
+vim.keymap.set('n', '-', '<cmd>Explore<CR>', { desc = 'Open file explorer (netrw)' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
